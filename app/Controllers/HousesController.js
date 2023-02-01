aed41c6230dfd91c4d27b7c9d1508e1e00574770
Jaden Hawkes <jadenhawkes@gmail.com>
2023-02-01 16:45:55 -0700

    Back to..somewhat working

--- app/Controllers/HousesController.js
+++ app/Controllers/HousesController.js
@@ -1,13 +1,39 @@
+import { appState } from "../AppState.js";
+import { House } from "../Models/House.js";
+import { housesService } from "../Services/HousesService.js";
+import { setHTML, setText } from "../Utils/Writer.js";
+import { Pop } from "../Utils/Pop.js";
 
+function _drawHouses(){
+  console.log('draw houses working!');
+  let template = ``
+  appState.house.forEach(h => template += h.HouseCardTemplate)
+  setHTML('listings', template)
+}
+
+function _drawHouse (){
+  console.log('your drew the selected house! too bad its not fully working yet');
+}
 export class HousesController {
 
 
   constructor() {
-    console.log('Hello this is the houses Controller')
+    console.log('Hello this is the houses Controller and I am surely working')
+    console.log(appState.house);
   }
 
   show(){
     console.log('TODO houses')
+    _drawHouses()
   }
 
+  
+  setHouse(houseId) {
+    try {
+      housesService.setHouse(houseId)
+      console.log('muh');
+    } catch (error) {
+      Pop.error(error)
+    }
+  }
 }
--- app/Models/House.js
+++ app/Models/House.js
@@ -1,3 +1,32 @@
+import { generateId } from "../Utils/generateId.js"
 
+export class House {
 
-export class House { }
+constructor(data){
+  this.id = generateId()
+   this.year = data.year
+   this.name = data.name
+   this.bedrooms = data.bedrooms
+   this.bathrooms = data.bathrooms
+   this.sqft = data.sqft
+   this.price = data.price
+   this.description = data.description
+   this.imgUrl = data.imgUrl
+}
+
+get HouseCardTemplate(){
+  return /*html*/ `
+  <div class="col-md-4 my-3">
+  <div class="card elevation-2 car" onclick="app.housesController.setHouse('${this.id}')" data-bs-toggle="modal" data-bs-target="#listingModal">
+    <img
+      src="${this.imgUrl}"
+      alt="${this.price}" class="rounded">
+    <p><b>${this.price} ${this.sqft} - $${this.price}</b></p>
+  </div>
+</div>
+  `
+}
+
+ }
+
+ 
