ed17eff7eddbefd651fd95864b21b34897c55ad5
Jaden Hawkes <jadenhawkes@gmail.com>
2023-02-01 16:18:37 -0700

    Some basic workings, some broken workings; startings

--- app/AppState.js
+++ app/AppState.js
@@ -3,6 +3,7 @@ import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 import { loadState } from "./Utils/Store.js"
+import { House } from "./Models/House.js"
 
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
@@ -12,6 +13,36 @@ class AppState extends EventEmitter {
   cars = loadState('cars', [Car])
   /** @type {import('./Models/Car').Car} */
   car = null
+
+  house = null
+
+
+  // house = [
+  //   new House(
+  //     {
+  //       name: 'Test House',
+  //       year: 2013,
+  //       bedrooms: 2,
+  //       bathrooms: 3,
+  //       sqft: 2130,
+  //       price: 200000,
+  //       description: 'A lovely test home that I love and have a lot of time and love for I love this omg wowzas!',
+  //       imgUrl: 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80'
+  //     }
+  //   ),
+  //   new House(
+  //     {
+  //       name: 'Sample House',
+  //       year: 2130,
+  //       bedrooms: 1,
+  //       bathrooms: 1,
+  //       sqft: 100,
+  //       price: 5,
+  //       description: 'A proper house sampler, it\'s got everything you need I promise',
+  //       imgUrl: 'https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2873&q=80'
+  //     }
+  //   )
+  // ]
 }
 
 export const appState = new Proxy(new AppState(), {
--- app/Services/HousesService.js
+++ app/Services/HousesService.js
@@ -1,4 +1,25 @@
-class HousesService { }
+import { appState } from "../AppState.js"
+import { House } from "../Models/House.js"
+import { saveState } from "../Utils/Store.js"
+
+class HousesService {
+
+  setActiveHouse(houseId){
+    const house = appState.house.find(h => h.id == houseId)
+    if (!house) {
+      throw new Error ('There is no darn house with thta ID buckaroo')
+    }
+    appState.house = house
+  }
+
+  setActiveCar(carId) {
+    const car = appState.cars.find(c => c.id == carId)
+    if (!car) {
+      throw new Error('there is no car with that id')
+    }
+    appState.car = car
+  }
+ }
 
 // singleton pattern more on this later
 export const housesService = new HousesService()
